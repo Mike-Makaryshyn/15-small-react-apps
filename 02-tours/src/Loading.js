762f17ca56abc336763051e9b3ddfc5ec39bde80
Mike Makaryshyn <mikemakaryshyn@gmail.com>
2021-09-21 17:42:26 +0300

    tours-project-completed

--- 02-tours/src/Loading.js
+++ 02-tours/src/Loading.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Loading = () => {
-  return (
-    <div className="loading">
-      <h1>loading...</h1>
-    </div>
-  );
+   return (
+      <div className="loading"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+   );
 };
 
 export default Loading;
